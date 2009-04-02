ab80f3cd812a63778fe5a857722be8b7517b8d5c
deeringj2 <deeringj2@2656ef14-ecf4-11dd-8fb1-9960f2a117f8>
2009-04-02 02:32:45 +0000

    Backing up/relocating changes that were made in Clean.

--- APIFramework/GFSprite.cpp
+++ APIFramework/GFSprite.cpp
@@ -12,49 +12,58 @@ GFSprite::~GFSprite() { };
 
 void GFSprite::changeBitmap(std::string name)
 {
-	std::cout << "CHANGE BITMAP???" << std::endl;
+	sprintf(_msgBuffer, "%d %d %s", SPRITE_CHANGE_BITMAP, _ref, name);
+	GameFramework::Instance().sendMessage(_msgBuffer);
+	_clrBuffer();
 };
 
 void GFSprite::setVisible(bool f)
 { 
 	sprintf(_msgBuffer, "%d %d %d", SPRITE_VISIBILITY_CHANGE, _ref, f); 
 	GameFramework::Instance().sendMessage(_msgBuffer);
+	_clrBuffer();
 };
 
 void GFSprite::setSpriteSize(int w, int h)
 { 
 	sprintf(_msgBuffer, "%d %d %d %d", SPRITE_SET_SIZE, _ref, w, h); 
 	GameFramework::Instance().sendMessage(_msgBuffer);
+	_clrBuffer();
 };
 
 void GFSprite::setSpritePosition(int x, int y)
 { 
 	sprintf(_msgBuffer, "%d %d %d %d", SPRITE_SET_LOCATION, _ref, x, y); 
 	GameFramework::Instance().sendMessage(_msgBuffer);
+	_clrBuffer();
 };
 
 void GFSprite::moveTo(int x, int y, int speed)
 { 
 	sprintf(_msgBuffer, "%d %d %d %d %d", SPRITE_MOVE_TO, _ref, x, y, speed); 
 	GameFramework::Instance().sendMessage(_msgBuffer);
+	_clrBuffer();
 };
 
 void GFSprite::setSpriteFrame(int frame)
 { 
 	sprintf(_msgBuffer, "%d %d %d", SPRITE_SET_FRAME, _ref, frame); 
 	GameFramework::Instance().sendMessage(_msgBuffer);
+	_clrBuffer();
 };
 
 void GFSprite::setAnimation(bool f)
 { 
 	sprintf(_msgBuffer, "%d %d %d", SPRITE_SET_ANIMATION, _ref, f); 
 	GameFramework::Instance().sendMessage(_msgBuffer);
+	_clrBuffer();
 };
 
 void GFSprite::setSpriteFrameDelay(int ticks)
 { 
 	sprintf(_msgBuffer, "%d %d %d", SPRITE_SET_FRAME_DELAY, _ref, ticks); 
 	GameFramework::Instance().sendMessage(_msgBuffer);
+	_clrBuffer();
 };
 
 /* PRINT FUNCTION */
